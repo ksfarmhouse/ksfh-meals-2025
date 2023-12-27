1fba8babbba85add29d090942a5df20cbf733897
Tyler Glenn <taglenn@ksu.edu>
2023-12-27 15:14:32 -0600

    add preliminary pages

---
+++ KsfhMeals/Website/Pages/PermaSignUp.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Website.Pages
+{
+    public class PermaSignUpModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
