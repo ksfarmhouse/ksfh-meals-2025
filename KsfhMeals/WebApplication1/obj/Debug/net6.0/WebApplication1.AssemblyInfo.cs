67985a84fcd16a7a5e8271c77ffc85cc786e8d4a
Hunter Rush <hunterjrush@gmail.com>
2025-12-02 14:52:52 -0600

    Added AI to help make adding menu easier

--- KsfhMeals/WebApplication1/obj/Debug/net6.0/WebApplication1.AssemblyInfo.cs
+++ KsfhMeals/WebApplication1/obj/Debug/net6.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+031d3eedcc6275e3be2f860033a5fcb0dda7161b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f90f4372b2061a586cebe7d14bfdf74f39f7cfcf")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
